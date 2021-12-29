d367e1ca9b8cce584acc7479395f62cf4617dadc
sauna1137 <subscription.search@gmail.com>
2021-12-29 21:58:14 +0900

    テスト用メソッド作成

--- PracticeUnitTest/ViewController.swift
+++ PracticeUnitTest/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    func add(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
 
 }
 
